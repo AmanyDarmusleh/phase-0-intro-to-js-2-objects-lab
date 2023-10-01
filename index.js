c77f4b688113b87df09ca8c9f0d35d85c94a60ab
AmanyDarmusleh <amanyaljemzawy@yahoo.com>
2023-10-01 16:00:18 +0300

    first quastion

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+const employee={
+    name:"Amany",
+    streetAddress:"xstreet",
+};
+
+ function updateEmployeeWithKeyAndValue(obj,key,value){
+     const newEmployee={...employee};
+ };
+
